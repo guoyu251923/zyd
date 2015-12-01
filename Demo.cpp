fc28a2f859af4d00a7dc27de4a9ec93250128ae9
yu.guo <yu.guo@acadine.com>
2015-12-01 11:26:29 +0800

    Demo.cpp modified 1st

--- Demo.cpp
+++ Demo.cpp
@@ -1,9 +1,15 @@
 include<iosteam>
+using namespace std;
 int main()
 {
 	cout<<"Demo Demo"<<endl;
 	cout<<"add"<<endl;
 	cout<<"Modified first time"<<endl;
 	cout<<"MOdified second time"<<endl;
+
+
+
+
+	cout<<"on subBranch brh----conflic:modified 1st"<<endl;
 	return 0;
 }
