f25be6dda46ab068dbc38165327507c7f9369f35
yu.guo <yu.guo@acadine.com>
2015-12-01 11:32:18 +0800

    Demo.cpp,Modified on Master branch 1st,for looking for conflic

--- Demo.cpp
+++ Demo.cpp
@@ -1,9 +1,13 @@
 include<iosteam>
+using namespace std;
 int main()
 {
 	cout<<"Demo Demo"<<endl;
 	cout<<"add"<<endl;
 	cout<<"Modified first time"<<endl;
 	cout<<"MOdified second time"<<endl;
+
+	cout<<"----------------"<<endl;
+	cout<<"It is on Master Branch,modified Demo.cpp also"<<endl;
 	return 0;
 }
